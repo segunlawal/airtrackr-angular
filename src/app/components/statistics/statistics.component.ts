05f03b54229f8da6090878011e9a236b922287f5
Segun <lawalsegun72@gmail.com>
2023-05-11 03:33:11 +0100

    add pagination for statistics page

--- src/app/components/statistics/statistics.component.ts
+++ src/app/components/statistics/statistics.component.ts
@@ -3,6 +3,7 @@ import { Subscription } from 'rxjs';
 import { Airports } from 'src/app/models/airports';
 import { FlightsApiService } from 'src/app/services/flights-api.service';
 import { UtilisService } from 'src/app/utils/utilis.service';
+import { PageEvent } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-statistics',
@@ -12,7 +13,10 @@ export class StatisticsComponent {
   private airportInfoChangedSubscription: Subscription;
 
   rangeInfo: Array<number>;
-  airportInfo: Array<Airports> = this.flightsService.getFlightsForAirports();
+  airportInfo: Array<Airports> = this.flightsService
+    .getFlightsForAirports()
+    .slice(0, 10);
+  public pageSlice: Array<Airports> = this.airportInfo.slice(0, 10);
 
   constructor(
     public utilsService: UtilisService,
@@ -21,13 +25,23 @@ export class StatisticsComponent {
     this.airportInfoChangedSubscription =
       this.flightsService.airportInfoChanged.subscribe((newState) => {
         this.airportInfo = newState;
-        // this.pageSlice = this.flights.slice(0, 10);
+        this.pageSlice = this.airportInfo.slice(0, 10);
       });
 
     const currentDate = Date.now() / 1000;
     const begin = Math.floor(currentDate - 3600);
     const end = Math.floor(currentDate + 3600);
     this.rangeInfo = [begin, end];
+  }
+
+  OnPageChange(event: PageEvent) {
+    // Handle pagination
+    const startIndex = event.pageIndex * event.pageSize;
+    let endIndex = startIndex + event.pageSize;
+    if (endIndex > this.airportInfo.length) {
+      endIndex = this.airportInfo.length;
+    }
+    this.pageSlice = this.airportInfo.slice(startIndex, endIndex);
   }
 
   ngOnDestroy() {
